0f27ded88e2f34f0eeffebc537ace7e251ad5411
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-06-30 21:33:02 +0300

    back dimitar

--- db/seedJobs.js
+++ db/seedJobs.js
@@ -9,7 +9,7 @@ async function seed() {
   // ? Always starts with mongodb://mongodb://127.0.0.1:27017/
   // ? Then you give it the name of the db to connect to (you make this up.)
   await connectToDB()
-  await mongoose.connection.db.dropDatabase()
+  // await mongoose.connection.db.dropDatabase()
   const newUser = await Users.findOne({ username: "" })
   JobData.forEach( post => {
     post.user = newUser
