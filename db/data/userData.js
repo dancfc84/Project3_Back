cb9b7394742becbf19d4f9871627ed9567cadba6
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-07-01 18:00:45 +0300

    addded dan user

--- db/data/userData.js
+++ db/data/userData.js
@@ -5,24 +5,29 @@ export default [
     email: "dumblevore@cern.ch",
     password: "44BatsBats$$",
     userType: "admin",
-
     profilePicture: "https://avatars.githubusercontent.com/u/100156884?v=4",
-
     bioUser: "but a humble jr dev",
-
     firstName: "Dimitar",
-
     lastName: "Vidolov",
-
     position: 'junior Dev at NewC',
-
     postsPosted: [],
-
     companyRepresented: "NewCo",
-
     jobsPosted: [],
-
     yearsExp: 0.25,
-
+  },
+  {
+    username: 'Dan',
+    email: "dan@hackertrees.co",
+    password: "44HatsHats$$",
+    userType: "admin",
+    profilePicture: "",
+    bioUser: "but a humble jr dev",
+    firstName: "Dan",
+    lastName: "Whittcok",
+    position: 'junior Dev at NewCo',
+    postsPosted: [],
+    companyRepresented: "NewCo",
+    jobsPosted: [3],
+    yearsExp: 15,
   }
 ]
