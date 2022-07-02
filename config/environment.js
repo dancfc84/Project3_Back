dd4a46e3970ad1f48525284084bfe61e4ad4a7b0
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-07-02 13:55:49 +0300

    deploy

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,8 @@
 //deployment
 import dotenv from 'dotenv'
 dotenv.config()
-export const dbURI =
-  process.env.DB_URI || 'mongodb://localhost/hackertrees'
+export const dbURL =
+  process.env.DB_URL || 'mongodb://localhost/hackertrees'
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'growTreesNoChopChopGottaSustain'
 
@@ -14,17 +14,13 @@ export const secret = process.env.SECRET || 'growTreesNoChopChopGottaSustain'
 
 //old ver
 
-
-
 // const environment = process.env.NODE_ENV
-
 
 // const mongoURL = "mongodb://127.0.0.1:27017/"
 
 // export const dbURL = environment === 'test'
 //   ? `${mongoURL}hackertrees-test`
 //   : `${mongoURL}hackertrees`
-
 
 // //secret for jwt
 // export const secret = "growTreesNoChopChopGottaSustain"
